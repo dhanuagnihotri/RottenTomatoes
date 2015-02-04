1691b7f0a74e0b6500efba242d8fc49c180f8784
Dhanu Agnihotri <dhanuagnihotri@gmail.com>
2015-02-04 14:10:12 -0800

    Initial Commit

---
+++ RottenTomatoesDhanu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenTomatoesDhanu
+//
+//  Created by Dhanu Agnihotri on 2/4/15.
+//  Copyright (c) 2015 ___SocietyTech___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenTomatoesDhanu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenTomatoesDhanu
+//
+//  Created by Dhanu Agnihotri on 2/4/15.
+//  Copyright (c) 2015 ___SocietyTech___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
